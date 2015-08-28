9971ee3aa83332624460b220ac9b47793c6ae802
jozadaquebatista <jozadaquebatista@gmail.com>
2015-08-28 00:06:56 -0300

    Get Input from user

---
+++ getInput.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class getInput
+{
+
+    public static void Main()
+    {
+
+        String name;
+	Console.WriteLine("Hello, say your name: ");
+	name = Console.ReadLine();
+	Console.WriteLine("Hello " + name + "!");
+
+    }
+
+}
